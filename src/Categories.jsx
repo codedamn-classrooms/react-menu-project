089094be82a3e175fb20bd8bb9efb346c1ca7c1a
Mehul Mohan <mehulmpt@gmail.com>
2020-12-24 02:44:40 +0530

    lab

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 
 const Categories = (props) => {
-	const filterItems = props.filterItems
+	const { filterItems, categories } = props
 
-	// Call filterItems(<NAME OF CATEGORY>) when button is clicked
-	// Example - <button className="filter-btn" onClick={() => filterItems('all')}>All</button>
 	return (
 		<div className="btn-container">
 			<button className="filter-btn" onClick={() => filterItems('all')}>
